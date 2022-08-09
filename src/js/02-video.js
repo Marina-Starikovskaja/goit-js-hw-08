ccd6fdc59272586f8de1f63a5dffb2c3a49ec382
Marina-Starikovskaja <marina.starikovskaja@gmail.com>
2022-08-09 08:12:12 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// отслеживает событие timeupdate чере з on
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// сохраняет данные в строку в секундах с помошью localStorage.setItem
+function onPlay ({seconds}) {
+    localStorage.setItem('videoplayer-current-time', seconds)
+};
+
+player.setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time'))
+)
+
 
