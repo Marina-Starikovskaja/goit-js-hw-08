3d5bd6b1e0b92f21fa64f0ddba3c251a2400e5b2
Marina-Starikovskaja <marina.starikovskaja@gmail.com>
2022-08-09 08:06:35 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryOfImg = document.querySelector('.gallery');
+
+
+// Создание галереии
+const itemEl = galleryItems
+    .map((image) =>
+        `<a class="gallery__item" href='${image.original}'>
+        <img class="gallery__image" src='${image.preview}' alt='${image.description}' loading="lazy"> 
+        </a>`)
+    .join("");
+
+galleryOfImg.insertAdjacentHTML('afterbegin', itemEl);
+
+// Слушатель события
+galleryOfImg.addEventListener('click', findImgAlt);
+function findImgAlt(event) {
+    return console.log(event.target.alt);
+}
+
+// Создание лайтбокс с помошью библиотеки SimpleLightbox
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250
+ });
+
+lightbox.on('show.SimpleLightbox');
 
 console.log(galleryItems);
