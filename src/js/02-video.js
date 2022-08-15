35fb36ad03d6a7a9d197affd186ff2b75e48b57c
Marina-Starikovskaja <marina.starikovskaja@gmail.com>
2022-08-15 15:55:29 +0300

    fixing bugs task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,55 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+// создание const на обработку ошибок
+const save = (key, value) => {
+  try {
+    localStorage.setItem(key, value);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const getItemKey = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const removeKey = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+
 
 // отслеживает событие timeupdate чере з on
 player.on('timeupdate', throttle(onPlay, 1000));
 
 // сохраняет данные в строку в секундах с помошью localStorage.setItem
 function onPlay ({seconds}) {
-    localStorage.setItem('videoplayer-current-time', seconds)
-};
+  save(VIDEOPLAYER_CURRENT_TIME, seconds);
+}
 
-player.setCurrentTime(
-    JSON.parse(localStorage.getItem('videoplayer-current-time'))
-)
+    if (getItemKey(VIDEOPLAYER_CURRENT_TIME)) {
+        player.setCurrentTime(getItemKey(VIDEOPLAYER_CURRENT_TIME));
+    };
+
+// player.setCurrentTime(
+//     JSON.parse(localStorage.getItem('videoplayer-current-time'))
+// )
 
 
